85e55cdaacd462a120783db80deced3d1d38f1ce
pujatB <76119090+pujatB@users.noreply.github.com>
2021-11-10 11:36:32 +0000

    url-shortener works with pyshortner

--- server.py
+++ server.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
-from templates import *
+from flask import Flask, render_template,request, redirect
+import pyshorteners
+import random
 
 app = Flask(__name__)
+
+def code():
+    code = ""
+    for i in range(5)
+        lon = random.choice(range(2))
+        if lon == 0:
+            code 
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+
 
 @app.route('/')
 def welcome():
     return "Welcome to url shortener!"
 
-@app.route('/shorten')
+@app.route('/shorten/', methods=['POST','GET'])
 def shorten():
-    return render_template('shorten.html')
+    if request.method == 'POST':
+        url = request.form['url']
+        s = pyshorteners.Shortener()
+        shorten_url = s.tinyurl.short(url)
+        print(shorten_url)
+        return render_template('shorten.html', shorten_url = shorten_url)
+    else:
+        return render_template('shorten.html', shorten_url="")
+
     
 
 if __name__ == '__main__':
     app.run(debug=True)
-
