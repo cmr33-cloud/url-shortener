4e01aea74d42809e42e2a38e5f7824d624123df0
pujatB <76119090+pujatB@users.noreply.github.com>
2021-11-10 15:24:15 +0000

    db working but not redirecting

--- server.py
+++ server.py
@@ -13,6 +13,10 @@ app.config["SQLAlchemy_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 class url(db.Model):
     _id = db.Column("id",db.Integer,primary_key=True)
     url = db.Column(db.String(100))
@@ -39,19 +43,19 @@ def code():
     return code 
 
 
-@app.route('/')
-def welcome():
-    return "Welcome to url shortener!"
+# @app.route('/')
+# def welcome():
+#     return "Welcome to url shortener!"
 
-@app.route('/shorten/', methods=['POST','GET'])
-def shorten():
+@app.route('/', methods=['POST','GET'])
+def url_shortener():
     if request.method == 'POST':
         user_url = request.form['url']
         # s = pyshorteners.Shortener()
         # shorten_url = s.tinyurl.short(user_url)
         shorten_url = code()
-        print(shorten_url)
-        new_url = url(url=user_url,shorten_url=shorten_url)
+        # print(f"short url  {shorten_url}")
+        new_url = url(url=user_url,shortened_url=shorten_url)
         db.session.add(new_url)
         db.session.commit()
         return render_template('shorten.html', shorten_url = shorten_url)
@@ -60,23 +64,19 @@ def shorten():
 
 @app.route('/<userURL>')
 def redirect(userURL):
-    new_url = url(url="testing.com",shortened_url="abcd")
-    db.session.add(new_url)
-    db.session.commit()
-    print(userURL)
-    url_data = url.query.all()
-    for urls in url_data:
-        print(urls.shortened_url, userURL)
-        if urls.shortened_url == userURL:
-            #   return "redirecting"
-              return redirect("google.co.uk")
-              break
-            
-        
+    long_url = url.query.filter_by(shortened_url=userURL).first()
+    # print(f"long url {long_url.url}")
+    if long_url:
+         return redirect("https://www.google.com"), 302
+        # return redirect(long_url.url)
+    else:
+        return "not found"
 
-
+    # all_url = url.query.all()
+    # for urls in all_url:
+    #     return f"{urls.url} and {urls.shortened_url}"
     
 
 if __name__ == '__main__':
-    db.create_all()
+    # db.create_all()
     app.run(debug=True)
