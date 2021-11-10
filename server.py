de6a1d42f5cefd8a83475eb60516754bb90181bb
Callum <80120239+cmr33-cloud@users.noreply.github.com>
2021-11-10 10:17:35 +0000

    add html

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from templates import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return "Welcome to url shortener!"
+
+@app.route('/shorten')
+def shorten():
+    return render_template('shorten.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
